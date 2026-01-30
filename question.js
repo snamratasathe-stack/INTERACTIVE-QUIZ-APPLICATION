2d277af675b98902acbb4f36151fad5791925b9e
snamratasathe-stack <snamratasathe@gmail.com>
2026-01-31 00:48:55 +0530

    Create question.js

---
+++ question.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+    question: "What does HTML stand for?",
+    options: ["Hyper Trainer Markup Language", "Hyper Text Markup Language", "High Text Machine Language", "None"],
+    answer: 1
+  },
+  {
+    question: "Which language styles web pages?",
+    options: ["HTML", "CSS", "Java", "Python"],
+    answer: 1
+  },
+  {
+    question: "Which is NOT a JavaScript framework?",
+    options: ["React", "Angular", "Vue", "Django"],
+    answer: 3
+  }
+];
